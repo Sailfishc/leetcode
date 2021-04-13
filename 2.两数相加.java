7144f90d69ef68adead385377d150eec182d23dd
sailfish <sailfishcc@gmail.com>
2021-04-13 17:33:57 +0800

    add two numbers solution

--- 2.两数相加.java
+++ 2.两数相加.java
@@ -21,63 +21,34 @@ class Solution {
     // 当前值：两数之和%10
     // 循环条件：两个链表都不为空
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode cur1 = l1;
-        ListNode cur2 = l2;
-        int delt = 0;
-        ListNode sumNode = new ListNode(0);
-        ListNode head = sumNode.next;
-        ListNode newNode = head;
-        while(cur1 != null && cur2 != null){
-            int sum = cur1.val + cur2.val + delt;
-            int val = sum %10;
-            delt = sum /10;
-            newNode = new ListNode(val);
-            newNode = newNode.next;
-            cur1 = cur1.next;
-            cur2 = cur2.next;
-        }
-        // 处理一个链表长一个短的情况
-        if(cur1 == null) {
-            // cur2还可以遍历
-            while(cur2 != null){
-                int sum = cur2.val + delt;
-                int val = sum %10;
-                delt = sum /10;
-                newNode = new ListNode(val);
-                newNode = newNode.next;
-                cur2 = cur2.next;
+        ListNode head = null;
+        ListNode tail = null;
+        int carry = 0;
+        while(l1 != null || l2 != null) {
+            int n1 = l1 == null ? 0 : l1.val;
+            int n2 = l2 == null ? 0 : l2.val;
+            int sum = n1+n2+ carry;
+            carry = sum / 10;
+            // 第一个元素
+            if(head == null){
+                head = tail = new ListNode(sum%10);
+            }else {
+                tail.next = new ListNode(sum%10);
+                tail = tail.next;
+            }
+            if(l1 != null){
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                l2 = l2.next;
             }
         }
-        if(cur2 == null) {
-            // cur1还可以遍历
-            while(cur1 != null){
-                int sum = cur1.val + delt;
-                int val = sum %10;
-                delt = sum /10;
-                newNode = new ListNode(val);
-                newNode = newNode.next;
-                cur1 = cur1.next;
-            }
+        if(carry > 0){
+            tail = new ListNode(carry);
         }
-        // 处理delt的进位问题
-        if (delt > 0){
-            newNode = new ListNode(delt);
-        }
-        // 反转链表
-        return reverseList(sumNode.next);
+        return head;
     }
 
-    public ListNode reverseList(ListNode head) {
-        ListNode pre = null;
-        ListNode cur = head;
-        while(cur != null) {
-            ListNode next = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = next;
-        }        
-        return pre;
-    }
 }
 // @lc code=end
 
