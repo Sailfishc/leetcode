bbf28bc8cdf659dec0520723f9adc72d8381ee51
sailfish <sailfishcc@gmail.com>
2021-04-14 10:26:42 +0800

    add leetcode 160 solution

---
+++ 160.相交链表.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=java
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    // 这个思路真的是很难想到，最常见的是Hash表的思路
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode h1 = headA;
+        ListNode h2 = headB;
+        while(h1 != h2) {
+            if(h1 != null){
+                h1 = h1.next;
+            }else {
+                h1 = headB;
+            }
+            if(h2 != null){
+                h2 = h2.next;
+            }else{
+                h2 = headA;
+            }
+        }
+        return h1;
+    }
+}
+// @lc code=end
+
